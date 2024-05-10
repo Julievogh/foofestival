900f6aa6d1ce971c75f7df93cc369373b9e8bfd7
August <augustjp@live.dk>
2024-05-10 11:31:43 +0200

    ticket component made, used in ticket-frontpage

---
+++ src/app/booking/components/TicketFrontpage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'next/link';
+
+ const TicketFrontpage = (props) => {
+
+  return (
+    <>
+    <div className='border-2 w-60 p-2 mb-3'>
+    <div className="flex justify-center items-center">
+  <h1 className='font-bold mb-2'> {props.ticketType}</h1>
+</div>
+        <div className='flex justify-around'>
+            <div>
+                <p>{props.camping}</p>
+                <p>{props.bath}</p>
+                <p>{props.toilets}</p>
+            </div>
+            <p>{props.price}</p>
+        </div>
+        <div class="flex justify-center">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2">
+    Buy ticket
+  </button>
+</div>
+
+    </div>
+    </>
+  )
+}
+export default TicketFrontpage
---
+++ src/app/booking/pages/ticket-frontpage/page.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import TicketFrontpage from '../../components/TicketFrontpage';
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="mb-8">
+        <h1 className="text-center text-3xl font-bold">Tickets</h1>
+      </div>
+      <div className="flex justify-center">
+        <TicketFrontpage
+          ticketType="Regular Ticket"
+          camping="Standard camping"
+          bath="Standard bath"
+          toilets="Box toilets"
+          price="799,-"
+        />
+      </div>
+      <div className="flex justify-center">
+        <TicketFrontpage
+          ticketType="Regular Ticket"
+          camping="Standard camping"
+          bath="Standard bath"
+          toilets="Box toilets"
+          price="799,-"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
