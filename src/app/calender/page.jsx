55bafd512b2ce47b00805bf63f2ffa5f185d3e2f
Julie Vogh <juvo0001@stud.kea.dk>
2024-05-17 11:47:19 +0200

    syncfusion

---
+++ src/app/calender/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Scheduler from "../components/Scheduler";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Schedule</h1>
+      <h3>See which bands are playing</h3>
+      <Scheduler />
+    </div>
+  );
+};
+
+export default HomePage;
