b991708e2888ada316eba5440a3d9eaf74cce74c
August <augustjp@live.dk>
2024-05-20 15:10:18 +0200

    started working on personal info forms

---
+++ src/app/components/PersonalInfoForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const PersonalInfo = () => {
+  return (
+    <form action="">
+        <label htmlFor="name">First Name:</label>
+        <input type="text" id='name' required />
+    </form>
+  )
+}
+
+export default PersonalInfo
--- src/app/pages/personal-info/page.jsx
+++ src/app/pages/personal-info/page.jsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import Timer from "@/app/components/Timer";
+import PersonalInfoForm from "@/app/components/PersonalInfoForm";
 
 const PersonalInfo = () => {
   return (
     <>
       <h3>Personal info</h3>
       <Timer duration={10 * 60 * 1000} />
+      <PersonalInfoForm />
     </>
   );
 };
