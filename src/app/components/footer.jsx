5c703361139617765031b185a0e690dc970b33ff
Julievogh <juvo0001@stud.kea.dk>
2024-05-24 09:59:26 +0200

    g

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./Footer.module.css";
-
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>Midlertidig Footer - virker den?</p>
+        <p>Midlertidig Footer</p>
         <p>Logo? Copyright? Names? Emails?</p>
       </div>
     </footer>
