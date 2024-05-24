9a452c73aca3b8cb188ecf177dfa493d9257be44
Julievogh <juvo0001@stud.kea.dk>
2024-05-24 10:03:31 +0200

    test uden footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,6 @@
-// app/layout.jsx
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className="content">
-          {children}
-          <Footer />
-        </div>
+        <div className="content">{children}</div>
       </body>
     </html>
   );
