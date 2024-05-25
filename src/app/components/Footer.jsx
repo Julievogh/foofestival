e1f9f4486f2a6dfeac8e24f4f551297bc8cf6085
Julievogh <juvo0001@stud.kea.dk>
2024-05-25 11:22:49 +0200

    footer igen

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>Follow Us on @FOOFEST</p>
+        <p>Made by Julie & August 2024 MMD Eksamen 3.sem</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
