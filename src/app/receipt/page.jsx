a9ba10c9d0ec5f1b6e753aaa19aef5154336abc2
August <augustjp@live.dk>
2024-05-29 12:07:11 +0200

    add receipt page - not done

---
+++ src/app/receipt/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const page = () => {
+  return (
+    <section>
+        <h1>FooFestival</h1>
+        <h2>Receipt</h2>
+        <p>Thanks for your order!</p>
+    </section>
+  )
+}
